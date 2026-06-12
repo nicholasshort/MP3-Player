a3654019b6c613ff3999f89e458e35902de810a8
nickshort <nicholas.r.h.short@gmail.com>
2026-06-12 00:02:00 -0400

    Working complete card initialization testing

--- MP3-Player-Firmware/DriversBoard/Src/sd_card_spi.c
+++ MP3-Player-Firmware/DriversBoard/Src/sd_card_spi.c
@@ -32,6 +32,14 @@ static inline bool txrx_byte(uint8_t tx, uint8_t* rx) {
 
 }
 
+static inline bool start_transaction(void) {
+
+    ncs_low();
+
+    return true;
+
+}
+
 static inline bool end_transaction(void) {
 
     ncs_high();
@@ -75,36 +83,52 @@ static bool send_dummy_clocks(void) {
 
 #define SD_DUMMY_READ_REQUESTS 16
 
-static bool send_command_packet_and_read_r1(uint8_t cmd, uint32_t argument, uint8_t crc, uint8_t* rx) {
+static bool send_command_packet_and_read_r1(uint8_t cmd, uint32_t argument, uint8_t crc, uint8_t* r1) {
+
+    if (r1 == NULL)
+        return false;
 
     uint8_t tx;
     
     // Send Command Byte
     tx = 0x40 | cmd;
-    if (!txrx_byte(tx, rx))
+    if (!txrx_byte(tx, r1))
         return false;
     
     for (uint8_t i = 0; i < 4; i++) {
         tx = (uint8_t)(argument >> (8*(3-i)));
-        if (!txrx_byte(tx, rx))
+        if (!txrx_byte(tx, r1))
             return false;  
     }
 
     tx = (crc | 0x01);
-    if (!txrx_byte(tx, rx))
+    if (!txrx_byte(tx, r1))
         return false;
 
     // Send DUMMY bytes waiting for R1 response byte
     for (uint8_t i = 0; i < SD_DUMMY_READ_REQUESTS; i++) {
-        if (!txrx_byte(SD_CARD_SPI_DUMMY_BYTE, rx))
+        if (!txrx_byte(SD_CARD_SPI_DUMMY_BYTE, r1))
             return false;
         
-        if (!(*rx >> 7))
+        if (!(*r1 >> 7))
             return true;
     }
 
-    *rx = 0xFF;
+    *r1 = 0xFF;
     return true;
+
+}
+
+static bool send_command_complete_transaction(uint8_t cmd, uint32_t argument, uint8_t crc, uint8_t* r1) {
+
+    if (r1 == NULL)
+        return false;
+    
+    bool ok = start_transaction();
+    ok &= send_command_packet_and_read_r1(cmd, argument, crc, r1);
+    ok &= end_transaction();
+
+    return ok;
 
 }
 
@@ -118,26 +142,55 @@ bool sd_card_spi_card_inserted() {
     return (HAL_GPIO_ReadPin(SD_CARD_NCD_GPIO_Port, SD_CARD_NCD_Pin) == false);
 }
 
+
+#define SD_CMD_CMD0                 0u
+#define SD_CMD0_ARG                 0u
+#define SD_CMD0_CRC                 0x95
+
+#define SD_CMD_CMD8                 8u
+#define SD_CMD8_ARG                 0x000001AAu
+#define SD_CMD8_CRC                 0x87u
+
+#define SD_CMD_CMD55                55u
+#define SD_CMD55_ARG                0u
+#define SD_CMD55_CRC                0xFFu
+
+#define SD_CMD_ACMD41               41u
+#define SD_ACMD41_ARG               0x40000000u
+#define SD_ACMD41_CRC               0xFFu
+
+#define SD_CMD_CMD58                58u
+#define SD_CMD58_ARG                0u
+#define SD_CMD58_CRC                0xFFu
+
+#define SD_R1_IDLE_STATE            0x01u
+#define SD_R1_READY_STATE           0x00u
+
+#define SD_INIT_HANDSHAKE_TIMEOUT_MS 1000u
+
 bool sd_card_spi_test() {
     
     bool ok = send_dummy_clocks();
     if (!ok)
         return false;
 
-    uint8_t rx;
+    uint8_t r1;
 
+    // --------------
+    // ENTER SPI MODE 
+    // --------------
     // Send CMD0
-    ncs_low();
-    ok &= send_command_packet_and_read_r1(0, 0, 0x95, &rx);
-    ok &= end_transaction();
-    if (rx != 0x01u || !ok) // Ensure R1 response byte
+    ok &= send_command_complete_transaction(SD_CMD_CMD0, SD_CMD0_ARG, SD_CMD0_CRC, &r1);
+    if (r1 != SD_R1_IDLE_STATE || !ok) // Ensure R1 response byte
         return false;
         
-    
+    // -------------
+    // VOLTAGE CHECK
+    // -------------
     // Send CMD8
-    ncs_low();
-    ok &= send_command_packet_and_read_r1(8, 0x000001AA, 0x87, &rx);
-    if (rx != 0x01u || !ok) {// Ensure R1 response byte
+    ok &= start_transaction();
+    ok &= send_command_packet_and_read_r1(SD_CMD_CMD8, SD_CMD8_ARG, SD_CMD8_CRC, &r1);
+    if (r1 != SD_R1_IDLE_STATE || !ok) {// Ensure R1 response byte
         end_transaction();
         return false;
     }
@@ -150,7 +203,55 @@ bool sd_card_spi_test() {
                  (r7_buffer[3] == 0xAA))) { // Ensure expected R7 response
             return false;
     }
-    
-    return ok;
+
+    // -----------------------------
+    // INITIALIZATION HANDSHAKE LOOP
+    // -----------------------------
+    uint32_t start_ms = HAL_GetTick();
+    do {
+        // Send CMD55
+        ok = send_command_complete_transaction(SD_CMD_CMD55, SD_CMD55_ARG, SD_CMD55_CRC, &r1);
+        if (!ok)
+            return false;
+        
+        if ((r1 != SD_R1_IDLE_STATE) && (r1 != SD_R1_READY_STATE))
+            return false;
+
+
+        // Send ACMD41
+        ok = send_command_complete_transaction(SD_CMD_ACMD41, SD_ACMD41_ARG, SD_ACMD41_CRC, &r1);
+        if (!ok)
+            return false;
+        
+        if (r1 == SD_R1_READY_STATE)
+            break; 
+        
+        if (r1 != SD_R1_IDLE_STATE)
+            return false;
+
+        // Check timeout
+        if ((HAL_GetTick() - start_ms) > SD_INIT_HANDSHAKE_TIMEOUT_MS)
+            return false;
+
+    } while (r1 == SD_R1_IDLE_STATE);
+
+    // -------------------
+    // Check Card Capacity
+    // -------------------
+    ok &= start_transaction();
+    ok &= send_command_packet_and_read_r1(SD_CMD_CMD58, SD_CMD58_ARG, SD_CMD58_CRC, &r1);
+    if (r1 != SD_R1_READY_STATE || !ok) {// Ensure R1 response byte
+        end_transaction();
+        return false;
+    }
+    uint8_t ocr[4];
+    ok &= read_buffer(ocr, 4);
+    ok &= end_transaction();
+    if (!ok)
+        return false;
+    bool high_extended_capacity = (ocr[0] & 0x40); // Check 30th bit for CCS
+
+
+    return true;
 
 }
